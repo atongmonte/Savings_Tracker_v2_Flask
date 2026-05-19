349116b9dff953dcff1126e8b78d63615d0f6d1c
atongmonte <atong@montefiore.org>
2026-05-19 09:57:14 -0400

    Migration update, adding fastCGI to webconfig

--- migration/DataMigrationprod_to_backup.sql
+++ migration/DataMigrationprod_to_backup.sql
@@ -1,13 +1,27 @@
 DECLARE @sql NVARCHAR(MAX) = N'';
 
+------------------------------------------------------------
+-- Step 1: Drop all existing tables in SavingsTracker_backup
+------------------------------------------------------------
 SELECT @sql = @sql + '
-IF OBJECT_ID(''[SavingsTracker_backup].[' + s.name + '].[' + t.name + ']'') IS NOT NULL
-    DROP TABLE [SavingsTracker_backup].[' + s.name + '].[' + t.name + '];
+DROP TABLE [SavingsTracker_backup].[' + s.name + '].[' + t.name + '];'
+FROM SavingsTracker_backup.sys.tables t
+JOIN SavingsTracker_backup.sys.schemas s
+    ON t.schema_id = s.schema_id
+WHERE s.name IN ('dbo','dev','prod','test');
 
+EXEC sp_executesql @sql;
+
+
+------------------------------------------------------------
+-- Step 2: Copy all tables from SavingsTracker to SavingsTracker_backup
+------------------------------------------------------------
+SET @sql = N'';
+
+SELECT @sql = @sql + '
 SELECT *
 INTO [SavingsTracker_backup].[' + s.name + '].[' + t.name + ']
-FROM [SavingsTracker].[' + s.name + '].[' + t.name + '];
-'
+FROM [SavingsTracker].[' + s.name + '].[' + t.name + '];'
 FROM SavingsTracker.sys.tables t
 JOIN SavingsTracker.sys.schemas s
     ON t.schema_id = s.schema_id
